6b941e095b1e203545c6d7e1e234af99ad265dc6
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-08-17 10:20:59 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JS here
+function contactPage() {
+    console.log("Waiting...");
+    window.open("pages/contact.html");
+    console.log("Completed...");
+}
+
+function changed() {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("success").style.display = "block";
+}
